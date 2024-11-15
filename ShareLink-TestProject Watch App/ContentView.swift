d3458154a0fec3ac937e82d8f0cf1c7ce071db97
Jonathon Lau <joncolours@gmail.com>
2024-11-15 18:54:18 +1030

    Initial Commit

---
+++ ShareLink-TestProject Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShareLink-TestProject Watch App
+//
+//  Created by Jonathon Lau on 15/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShareLink-TestProject Watch App/ShareLink_TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareLink_TestProjectApp.swift
+//  ShareLink-TestProject Watch App
+//
+//  Created by Jonathon Lau on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShareLink_TestProject_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
